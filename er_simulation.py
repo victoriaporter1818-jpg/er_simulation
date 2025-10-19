9c7774a15077ee6b833dbe7b0cd6a9786a4612e9
victoriaporter1818-jpg <victoriaporter1818@gmail.com>
2025-10-19 01:31:52 -0400

    Update er_simulation.py

--- er_simulation.py
+++ er_simulation.py
@@ -177,8 +177,8 @@ with col2:
                 ["-- Select --", "Discharge", "Send to Surgery", "Send to ICU"],
                 key="transfer_destination"
             )
-        if st.button("Confirm Transfer", key="confirm_transfer"):
-    with st.expander("🏁 Patient Transfer Summary", expanded=True):
+            if st.button("Confirm Transfer", key="confirm_transfer"):
+        with st.expander("🏁 Patient Transfer Summary", expanded=True):
         total_score = max(0, min(100, int(st.session_state.score)))
         effectiveness = total_score
 
